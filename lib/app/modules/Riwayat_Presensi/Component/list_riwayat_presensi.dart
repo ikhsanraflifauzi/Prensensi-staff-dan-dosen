da65bc077e21cd8e789db32ef9a5964387bb268f
unknown <ikhsanrafli10@gmail.com>
2023-06-01 13:48:49 +0700

    update riwayat presensi

--- lib/app/modules/Riwayat_Presensi/Component/list_riwayat_presensi.dart
+++ lib/app/modules/Riwayat_Presensi/Component/list_riwayat_presensi.dart
@@ -1,3 +1,4 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
@@ -25,91 +26,112 @@ class ListRiwayat extends GetView<RiwayatPresensiController> {
           ),
         ),
         Expanded(
-          child: ListView.builder(
-            shrinkWrap: false,
-            itemCount: 20,
-            // physics: NeverScrollableScrollPhysics(),
-            itemBuilder: (context, index) {
-              return Padding(
-                padding: const EdgeInsets.all(20),
-                child: Container(
-                  width: 303,
-                  height: 113,
-                  decoration: BoxDecoration(
-                    color: Colors.white,
-                    borderRadius: BorderRadius.circular(15),
-                    boxShadow: [
-                      BoxShadow(
-                          offset: Offset(0, 7),
-                          blurRadius: 7,
-                          color: Colors.grey),
-                    ],
-                  ),
-                  child: Padding(
-                    padding: const EdgeInsets.all(8),
-                    child: Column(children: [
-                      SizedBox(
-                        height: 10,
+          child: StreamBuilder<QuerySnapshot<Map<String, dynamic>>>(
+              stream: controller.streamAllPresensi(),
+              builder: (context, snap) {
+                if (snap.connectionState == ConnectionState.waiting) {
+                  return Center(child: CircularProgressIndicator());
+                }
+                if (snap.data?.docs.length == 0 || snap.data == null) {
+                  return SizedBox(
+                    height: 200,
+                    child: Center(
+                      child: Text(
+                        'belum ada data presensi',
+                        style: TextStyle(fontFamily: 'Lexend', fontSize: 14),
                       ),
-                      Row(
-                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                        children: [
-                          Text(
-                            'Check in ',
-                            style: TextStyle(
-                                fontFamily: 'Lexend',
-                                fontSize: 14,
-                                fontWeight: FontWeight.w500),
-                          ),
-                          Text('${DateFormat.yMMMEd().format(DateTime.now())}'),
-                        ],
+                    ),
+                  );
+                }
+                return ListView.builder(
+                  shrinkWrap: false,
+                  itemCount: snap.data!.docs.length,
+                  // physics: NeverScrollableScrollPhysics(),
+                  itemBuilder: (context, index) {
+                    Map<String, dynamic>? data =
+                        snap.data!.docs.reversed.toList()[index].data();
+                    return Padding(
+                      padding: const EdgeInsets.all(20),
+                      child: Container(
+                        width: 303,
+                        height: 113,
+                        decoration: BoxDecoration(
+                          color: Colors.white,
+                          borderRadius: BorderRadius.circular(15),
+                          boxShadow: [
+                            BoxShadow(
+                                offset: Offset(0, 7),
+                                blurRadius: 7,
+                                color: Colors.grey),
+                          ],
+                        ),
+                        child: Padding(
+                          padding: const EdgeInsets.all(8),
+                          child: Column(children: [
+                            SizedBox(
+                              height: 10,
+                            ),
+                            Row(
+                              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                              children: [
+                                Text(
+                                  'Check in ',
+                                  style: TextStyle(
+                                      fontFamily: 'Lexend',
+                                      fontSize: 14,
+                                      fontWeight: FontWeight.w500),
+                                ),
+                                Text(
+                                    '${DateFormat.yMMMEd().format(DateTime.now())}'),
+                              ],
+                            ),
+                            Row(
+                              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                              children: [
+                                Text(
+                                  "${DateFormat.Hms().format(DateTime.now())}",
+                                  style: TextStyle(
+                                      fontFamily: 'Lexend',
+                                      fontSize: 14,
+                                      fontWeight: FontWeight.w500),
+                                ),
+                                Text('Status : masuk'),
+                              ],
+                            ),
+                            SizedBox(
+                              height: 8,
+                            ),
+                            Row(
+                              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                              children: const [
+                                Text(
+                                  'Chek Out',
+                                  style: TextStyle(
+                                      fontFamily: 'Lexend',
+                                      fontSize: 14,
+                                      fontWeight: FontWeight.w500),
+                                ),
+                              ],
+                            ),
+                            Row(
+                              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                              children: [
+                                Text(
+                                  "${DateFormat.Hms().format(DateTime.now())}",
+                                  style: TextStyle(
+                                      fontFamily: 'Lexend',
+                                      fontSize: 14,
+                                      fontWeight: FontWeight.w500),
+                                ),
+                              ],
+                            ),
+                          ]),
+                        ),
                       ),
-                      Row(
-                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                        children: [
-                          Text(
-                            "${DateFormat.Hms().format(DateTime.now())}",
-                            style: TextStyle(
-                                fontFamily: 'Lexend',
-                                fontSize: 14,
-                                fontWeight: FontWeight.w500),
-                          ),
-                          Text('Status : masuk'),
-                        ],
-                      ),
-                      SizedBox(
-                        height: 8,
-                      ),
-                      Row(
-                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                        children: const [
-                          Text(
-                            'Chek Out',
-                            style: TextStyle(
-                                fontFamily: 'Lexend',
-                                fontSize: 14,
-                                fontWeight: FontWeight.w500),
-                          ),
-                        ],
-                      ),
-                      Row(
-                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                        children: [
-                          Text(
-                            "${DateFormat.Hms().format(DateTime.now())}",
-                            style: TextStyle(
-                                fontFamily: 'Lexend',
-                                fontSize: 14,
-                                fontWeight: FontWeight.w500),
-                          ),
-                        ],
-                      ),
-                    ]),
-                  ),
-                ),
-              );
-            },
-          ),
+                    );
+                  },
+                );
+              }),
         ),
       ],
     );
--- lib/app/modules/Riwayat_Presensi/controllers/riwayat_presensi_controller.dart
+++ lib/app/modules/Riwayat_Presensi/controllers/riwayat_presensi_controller.dart
@@ -1,6 +1,21 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:simple_navigation_menu/simple_navigation_menu.dart';
 
-class RiwayatPresensiController extends GetxController {}
+class RiwayatPresensiController extends GetxController {
+  FirebaseAuth auth = FirebaseAuth.instance;
+  FirebaseFirestore fireStore = FirebaseFirestore.instance;
+
+  Stream<QuerySnapshot<Map<String, dynamic>>> streamAllPresensi() async* {
+    String uid = auth.currentUser!.uid;
+    yield* fireStore
+        .collection('Employee')
+        .doc(uid)
+        .collection("presensi")
+        .orderBy("tanggal")
+        .snapshots();
+  }
+}
